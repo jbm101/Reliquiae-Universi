3ec25862662a1e78782ec04d2cc3938b7ba26c43
jbm101 <30279819+jbm101@users.noreply.github.com>
2026-04-09 03:32:57 -0500

    Quartz sync: Apr 9, 2026, 3:32 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jbm101.github.io/Reliquiae-Universi/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
